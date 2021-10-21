e43b1e6f8867030acc2d9a0efb3e771306f554dc
Kieran C <kieranakc2@gmail.com>
2021-10-21 23:16:56 +0100

    Intro about me added

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,19 @@ export default function Index(props) {
       <section id="intro">
         <div>
           <h1>Hi, I'm Kieran Cyprien</h1>
-          <p>I'm an aspiring web developer / javascript programmer in London</p>
+          <p>I'm an aspiring web developer / programmer in London</p>
+          <p>
+            I'm currently mostly into front end development with a strong desire
+            to become as proficient and skilled a programmer as possible. I have
+            aspirations to learn more languages and frameworks / libraries with
+            the ultimate goal to become a full stack developer and mobile dev.
+            I'm currently working and skilled with React, Js and SASS.
+          </p>
+          <p>
+            With my current skillset I would like to take my skillset to a
+            professional setting with your company, with which I beleive I would
+            be an exccellent candidate.
+          </p>
         </div>
       </section>
     </>
